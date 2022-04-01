eac368284a02d1d9c0629ca3d38bf3f12b29e350
fe <gansoishiyaki@gmail.com>
2022-04-02 04:55:19 +0900

    initial commit

---
+++ Assets/Scripts/Models/Card.cs
@@ -0,0 +1,41 @@
+using System.Collections;
+using System.Collections.Generic;
+using YamlDotNet.Serialization;
+using YamlDotNet.Serialization.NamingConventions;
+
+public class Card
+{
+    public enum Type
+    {
+        Passive,
+        Equipe,
+        Skill,
+        Tool
+    }
+
+    public enum Attribute
+    {
+        Union
+    }
+
+    public string id;
+    public string name;
+    public string title;
+    public string description;
+
+    public int maxHP;
+    public int interigence;
+    public int str;
+    public int spd;
+    public int dex;
+    public int luck;
+
+    public List<Attribute> attributes;
+    public Type type;
+
+    // Deserialized
+    [YamlMember(Alias ="type")]
+    public string _type;
+    [YamlMember(Alias = "attributes")]
+    public List<string> _attributes;
+}
---
+++ Assets/Scripts/Models/User.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+
+public class User
+{
+    public string id;
+    public string name;
+    public List<string> cards;
+    public Card equip;
+
+    public int hp;
+    public int coin;
+
+    public int maxHP;
+    public int interigence;
+    public int str;
+    public int spd;
+    public int dex;
+    public int luck;
+}
